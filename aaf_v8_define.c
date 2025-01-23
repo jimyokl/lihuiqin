d9b23aaea88637f985e02d9440c4ba8057418a89
yan <yannetwork@qq.com>
2025-01-23 20:30:09 +0800

    Jan23 lib end

--- aaf_v8_define.c
+++ aaf_v8_define.c
@@ -2,9 +2,10 @@
 #define PI 3.1415926
 #define ADD 2+3
 //#define MAX(a,b) ((a)>(b)?(a):(b))  // 用括号括起来,防止出现问题
-#define MAX(a,b)  a>b?a:b             // (a>b?a:b) is better, 带参的宏定义
+#define MAX(a,b)  (a)>(b)?(a):(b)             // (a>b?a:b) is better, 带参的宏定义
 #define MAX2(a,b)  \
-            {int A=a,B=b; ((A) > (B) ? (A) : (B));}
+            ( {int A=(a),B=(b); ((A) > (B) ? (A) : (B));} )
+            //{int A=(a),B=(b); ((A) > (B) ? (A) : (B));} // error: expected expression before ‘{’ token             
 
 /* ------------------------------part 1 define------------------------------ */
 
@@ -30,7 +31,16 @@ int main()
 
     printf("before running MAX(i++,j++), i = %d\tj = %d\n",i,j);
     printf("MAX(i++,j++) = %d\n", MAX(i++,j++));  // i>j?i:j
+    //预编译,gcc -E后变为: printf("MAX(i++,j++) = %d\n", (i++)>(j++)?(i++):(j++));
+    //需要谨慎使用 define, 这里就出现了 i++ 两次,导致结果不是预期的
     printf("after running MAX(i++,j++), i = %d\tj = %d\n",i,j);
+
+    // 带参的宏
+    int k = 5, l = 3;
+    printf("before running MAX2(k++,l++), k = %d\tl = %d\n",k,l);
+    printf("MAX2(k,l) = %d\n", MAX2(k++,l++));  // k>l?k:l
+    //预编译后变为: printf("MAX2(k,l) = %d\n", ( {int A=(k++),B=(l++); ((A) > (B) ? (A) : (B));} ));
+    //这里A接收了k的值,所以没有++自增
 
     // to del 三目条件运算符（conditional ternary operator）
     int toDel = 0;
