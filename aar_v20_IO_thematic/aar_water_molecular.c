fdaa9fdf805161fa168e9a2ceb963ba9cf8fd85d
yan <yannetwork@qq.com>
2025-03-26 21:50:42 +0800

    mar26 end

--- aar_v20_IO_thematic/aar_water_molecular.c
+++ aar_v20_IO_thematic/aar_water_molecular.c
@@ -13,7 +13,7 @@ int main( )
     // root( );
     exit(0);
 }
-static void water(void)
+static void water(void)        // static禁止函数在其他文件中使用（外部扩展）
 {
     float num; double sum;
     printf("Please input how many quart of water:");
@@ -26,6 +26,11 @@ static void water(void)
     sum = num*QUART/WATER_MOLECULAR_WEIGHT;
     printf("sizeof(float) = %lu\n", sizeof(float));
     printf("Total water molecular number is %f\n", sum); //31,666,666,666,666,665,598,517,248
+    //float 可以表示非常大的数（比如 3.4 × 10³⁸）。例如，最大值 3.402823466e+38, 
+    //假设float用32位存储数据,1 位符号位,8 位指数部分,23 位尾数部分,
+    //浮点数的表示方式能够通过使用 指数部分 来扩展数值的范围
+    //浮点数的指数部分可以表示正负指数，所以它能够表示非常大的或非常小的数。eg指数部分为127时，表示 2^127,这个数非常大
+    //对于最大值，指数部分的最大值是 254（二进制表示），表示的数值大约是 2^128。
     printf("Total water molecular number is %e\n", sum);
     return;
 }
