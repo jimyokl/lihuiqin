0e2eacdfa271eebe8f5f03ae30fd73a2547cd4b6
yan <yannetwork@qq.com>
2025-04-18 12:35:07 +0800

    april17 end

--- abf_v30_3x_array/abi_v31_thematic_exercise/abm_v34_num_base_conversion.c
+++ abf_v30_3x_array/abi_v31_thematic_exercise/abm_v34_num_base_conversion.c
@@ -2,11 +2,30 @@
 // It refers to converting a number from one base (e.g., decimal, binary, hexadecimal) to another.
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX 64
 static void base_convert()
 {
-    
+    int num=0, base=0, digit_n[MAX], i=0;
 
+    printf("Please input a number and a base (2-16): ");
+    scanf("%d %d", &num, &base);
+    if(num<0 || base<2 || base>16)
+        printf("Invalid input! Please try again.\n");
+
+    do{
+        digit_n[i]=num%base;
+        num=num/base;
+        i++;
+    //}while(num!=0);
+    }while(num>0);
+    for(i--;i>=0;i--)
+    {
+        if(digit_n[i]<10)
+            printf("%d ", digit_n[i]);
+        else
+            printf("%c", digit_n[i]-10+'A');
+    }
+    printf("\n");
     return;
 }
 int main()
