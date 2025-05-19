744151b747b260ba424f62f2a1f89c34e73d0c59
yan <yannetwork@qq.com>
2025-05-19 21:19:08 +0800

    may19 done

---
+++ aaa_data_type_var_operator/xzz_ascII.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("char '0'\t=%d\n",'0');          // =48
+
+    printf("char '\\0'\t=%d\n",'\0');       // =0
+
+    exit(0);
+}
